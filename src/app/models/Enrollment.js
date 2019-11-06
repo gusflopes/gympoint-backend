e3b1cf0dd7c228ed0ee4f81d0f7c28b484ae6855
gusflopes <gusflopes86@gmail.com>
2019-11-06 10:30:50 -0400

    feat: Enrollment - Active as Virtual Field

--- src/app/models/Enrollment.js
+++ src/app/models/Enrollment.js
@@ -1,4 +1,5 @@
-import Sequelize, { Model } from 'sequelize';
+import Sequelize, { Model, DataTypes } from 'sequelize';
+import {} from 'date-fns';
 
 class Enrollment extends Model {
   static init(sequelize) {
@@ -9,6 +10,22 @@ class Enrollment extends Model {
         start_date: Sequelize.DATE,
         end_date: Sequelize.DATE,
         price: Sequelize.FLOAT,
+        active: {
+          type: new DataTypes.VIRTUAL(DataTypes.BOOLEAN, [
+            'end_date',
+            'start_date',
+          ]),
+          get() {
+            const today = new Date();
+            if (
+              today <= this.get('end_date') &&
+              today >= this.get('start_date')
+            ) {
+              return true;
+            }
+            return false;
+          },
+        },
       },
       {
         sequelize,
