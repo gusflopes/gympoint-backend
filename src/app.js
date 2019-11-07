06db690e28ac07ff33b08be4942f84dd5e18b814
gusflopes <gusflopes86@gmail.com>
2019-11-07 08:59:59 -0400

    feat: Cors and Sentry Implementation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
+import 'dotenv/config';
+
 import express from 'express';
+import cors from 'cors';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
+
 import routes from './routes';
+import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -7,16 +15,39 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
   }
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  // IMPORTANT! The SENTRY_DSN must be provided on environment variables
+  // Else, all Sentry references must be commented
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        // const errors = await new Youch(err, req).toHTML();
+        console.log('Erro no exceptionHandler');
+
+        const errors = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal server error' });
+    });
   }
 }
 
